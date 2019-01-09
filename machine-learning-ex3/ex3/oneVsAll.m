96ce39580d3efe0b1a6abccdd2093edaaa1f4e2e
Charles Van Tassel <46469084+cvantass@users.noreply.github.com>
2019-01-09 13:50:15 -0500

    Update oneVsAll.m

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -55,17 +55,6 @@ options = optimset('GradObj', 'on', 'MaxIter', 50);
 [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
 all_theta(c,:) = [theta];
 
-
-
-
-
-
-
-
-
-
-
-
 % =========================================================================
 
 
